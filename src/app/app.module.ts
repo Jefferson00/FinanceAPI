9433de7c2eccf92e18574316abbe761e9d9dced5
Jefferson00 <jeffersonts00@gmail.com>
2022-02-20 20:07:04 -0300

    feat: users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,9 @@ import { UsersModule } from './../models/users/users.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot({
-      include: [UsersModule],
-      autoSchemaFile: 'schema.gql',
-      buildSchemaOptions: {
-        dateScalarMode: 'isoDate',
-      },
-    }),
-    UsersModule,
-  ],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/models/users/users.module.ts
+++ src/models/users/users.module.ts
@@ -3,11 +3,10 @@ import { UsersController } from './users.controller';
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/providers/database/prisma/prisma.service';
 import { UsersService } from './users.service';
-import { UsersResolver } from './users.resolver';
 
 @Module({
     imports: [],
     controllers: [UsersController],
-    providers: [UsersService, PrismaService, UsersResolver],
+    providers: [UsersService, PrismaService],
 })
 export class UsersModule {}
